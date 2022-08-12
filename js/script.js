5972816827200695f7f1e909df6e086bd6a219b6
Yusif Bayramov <byusub2@gmail.com>
2022-08-12 20:27:12 +0400

    circle timer

--- js/script.js
+++ js/script.js
@@ -1,3 +1,33 @@
+function startTimer(duration) {
+
+    var timeout = setTimeout(function() {
+        var time = duration;
+        var i = 1;
+        var k = ((i / duration) * 100);
+        var l = 100 - k;
+        i++;
+        document.getElementById("c1").style.strokeDasharray = [l, k];
+        document.getElementById("c2").style.strokeDasharray = [k, l];
+        document.getElementById("c1").style.strokeDashoffset = l;
+        document.getElementById("counterText").innerHTML = duration;
+        var interval = setInterval(function() {
+            if (i > time) {
+                clearInterval(interval);
+                clearTimeout(timeout);
+                return;
+            }
+            k = ((i / duration) * 100);
+            l = 100 - k;
+            document.getElementById("c1").style.strokeDasharray = [l, k];
+            document.getElementById("c2").style.strokeDasharray = [k, l];
+            document.getElementById("c1").style.strokeDashoffset = l;
+            console.log(k, l);
+            document.getElementById("counterText").innerHTML = (duration + 0) - i;
+            i++;
+        }, 1000);
+    }, 0);
+}
+startTimer(5);
 const menuBtn = document.querySelector(".menu-icon span");
 
 const cancelBtn = document.querySelector(".cancel-icon");
